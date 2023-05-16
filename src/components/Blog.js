6841b30ed1c9e99e20d918a42d20f90d7c7cf66f
Staffan Småros <staffan.smaros@gmail.com>
2023-05-16 14:00:41 +0300

    Completed exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,30 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [infoVisible, setinfoVisble] = useState(false)
+
+  const showWhenVisibile = { display: infoVisible ? '' : 'none'}
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const showAdditionalInfo = () => setinfoVisble(!infoVisible)
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={showAdditionalInfo}>{infoVisible ? 'Hide' : 'Show'}</button>
+      <div style={showWhenVisibile}>
+        {blog.url}<br />
+        likes {blog.likes} <button>like</button><br />
+        {blog.user.name}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
