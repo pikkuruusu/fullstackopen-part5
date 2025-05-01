c9ad231336f2f1d8ecc99bdb552e52678e2f6249
Staffan Småros <staffan.smaros@gmail.com>
2025-05-01 14:04:17 +0300

    Completed exercise 5.18

--- fullstackopen-part5-playwright/tests/blog-app.spec.js
+++ fullstackopen-part5-playwright/tests/blog-app.spec.js
@@ -1,16 +1,40 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 const exp = require('constants')
+const { loginWith } = require('./helper')
 
 describe('Blog app', () => {
-    beforeEach(async ({ page }) => {
+    beforeEach(async ({ page, request }) => {
+        await request.post('http://localhost:3003/api/testing/reset')
+        await request.post('http://localhost:3003/api/users', {
+            data: {
+                username: 'test-user',
+                name: 'Staffan Småros',
+                password: 'hemligt'
+            }
+        })
+
         await page.goto('http://localhost:5173')
     })
 
     test('Login form is shown', async ({ page }) => {
         await expect(page.getByText('username')).toBeVisible()
-        await expect(page.locator('[name="Username"]')).toBeVisible()
+        await expect(page.getByTestId('username')).toBeVisible()
         await expect(page.getByText('password')).toBeVisible()
-        await expect(page.locator('[name="Password"]')).toBeVisible()
+        await expect(page.getByTestId('password')).toBeVisible()
         await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+    })
+
+    describe('Login', () => {
+        test('succeeds with correct credentials', async ({ page }) => {
+            await loginWith(page, 'test-user', 'hemligt')
+
+            await expect(page.getByText('Staffan Småros is logged in')).toBeVisible()
+        })
+
+        test('fails with wrong credentials', async ({ page }) => {
+            await loginWith(page, 'test-user', 'wrong')
+
+            await expect(page.locator('.notification')).toHaveText('Wrong credentials')
+        })
     })
 })
